7dea45ed67f234afb389a0bbeb0caf9a7c2a8e1a
chrsrbnsn <chrs.rbnsn@gmail.com>
2025-06-25 00:43:25 -0500

    adding .config folder

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,14 @@
+-- OPTIONS
+local set = vim.opt
+
+--line nums
+set.relativenumber = true
+set.number = true
+
+-- indentation and tabs
+set.tabstop = 4
+set.shiftwidth = 4
+set.autoindent = true
+set.expandtab = true
+
+
